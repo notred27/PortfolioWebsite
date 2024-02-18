8061ef4e49d2a2a915ee5661e540a841bd5f5a22
notred27 <mikereidy95@gmail.com>
2024-02-18 16:07:16 -0500

    Updated UI

---
+++ src/Header.js
@@ -0,0 +1,32 @@
+
+
+export default function Header({title, color, side}) {
+    if (side == "right"){
+        return (
+            <div style={{display:"flex", width:"100%", justifyContent:"right"}}>
+                <div style={{height:"fit-content", display:"flex", flexDirection:"column", width:"30vw", minWidth:"fit-content", float:"right"}}>
+                    <div style={{zIndex:"1", position:"relative", backgroundColor:"#1E1E1E", width:"97%", marginLeft:"3%", height:"100%", borderBottomLeftRadius:"30px", borderTopLeftRadius:"30px", textAlign:"left"}}>
+                    {/* <div style={{zIndex:"1", position:"relative", backgroundColor:"#1E1E1E", width:"97%", marginLeft:"3%", height:"100%", borderBottomLeftRadius:"30px", borderTopLeftRadius:"30px", textAlign:"left", boxShadow:`-15px 15px ${color}`}}>  */}
+
+                        <h1 style={{color:"white", float:"left", margin:"10px", marginLeft:"30px",paddingRight:"30px", whiteSpace:"nowrap"}}>{title}</h1>
+                    </div>
+    
+                    <div style={{zIndex:"0",position:"relative",backgroundColor:`${color}`, width:"100%", height:"50px", top:"-30px", borderBottomLeftRadius:"30px", borderTopLeftRadius:"30px"}} />
+                </div>
+            </div>
+        );
+    } else {
+        return (
+        <div style={{display:"flex", width:"100%", justifyContent:"left"}}>
+            <div style={{height:"fit-content", display:"flex", flexDirection:"column", width:"30vw", minWidth:"fit-content"}}>
+                <div style={{zIndex:"1", position:"relative", backgroundColor:"#1E1E1E", width:"100%", height:"100%", borderBottomRightRadius:"30px", borderTopRightRadius:"30px", textAlign:"left"}}> 
+                    <h1 style={{color:"white", float:"right", margin:"10px", marginRight:"30px",paddingLeft:"30px", whiteSpace:"nowrap"}}>{title}</h1>
+                </div>
+
+                <div style={{zIndex:"0",position:"relative",backgroundColor:`${color}`, width:"95%", height:"50px", top:"-30px", borderBottomRightRadius:"30px"}}/>
+            </div>
+        </div>
+        );
+    }
+    
+}
